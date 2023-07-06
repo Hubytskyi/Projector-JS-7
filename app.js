95803f8b3706d9787f81be5273417c38acc2e31f
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-07-06 20:54:35 +0300

    add primitive types in details

---
+++ app.js
@@ -0,0 +1,118 @@
+"use strict";
+
+// console.log(typeof null);
+
+// const year = 1995;
+// console.log("JavaScript was invented by Brendan Eich in ${year}");
+// console.log(`JavaScript was invented by Brendan Eich in ${year}`);
+
+// console.log(19 % 5);
+// console.log(2 ** 4);
+
+// console.log(10 != "10");
+// console.log(10 == "10");
+
+// console.log(10 === "10");
+
+// MATH min max
+
+// NUMBER
+// console.log(Number(" 10 "));
+// console.log("string");
+// console.log(Number(""));
+
+// console.log(+" 10 ");
+// console.log(+"");
+
+// console.log(+"010");
+// console.log(+"010w");
+
+// console.log(parseInt("100px"));
+// console.log(parseInt("0100px"));
+// console.log(parseFloat("100.62px"));
+
+// STRING
+// const num = 42;
+// console.log(num.toString());
+
+// console.log(10.toString())
+
+// const data = null;
+// console.log(null.toString());
+// console.log(undefined.toString());
+// console.log(40..toString());
+// console.log(String(42));
+// console.log(10 + "");
+
+// BOOLEAN
+// console.log(Boolean("text"));
+// console.log(Boolean(42));
+
+// console.log(!!"");
+// console.log(!!0);
+// console.log(!!null);
+
+// const data = {
+//   user: "some user",
+// };
+
+// const user = data.user ?? "exist";
+
+// console.log(BigInt("10"));
+
+// console.log(true + 12 + "hello" + null);
+// console.log(10 * 2 * true);
+// console.log(10 * 2 + null + "20");
+// console.log(10 * 2 + null * "20");
+// console.log(10 * true + 20 * false);
+// console.log(10 * 5 - null * "42" + "10" + 5);
+
+// let user = 0;
+// const userName1 = user.name || "Анонім" || null || 0;
+// const userName2 = user.name || "Анонім";
+
+// console.log(userName1);
+
+// const userName = user ?? "exist";
+// console.log(userName);
+
+// let showUserName = true;
+// const userName2 = "Dmytro";
+// const userNameForRendering = showUserName && true && "dasdasd" && userName2;
+// const userNameForRendering2 = showUserName && false && "dasdasd" && userName2;
+
+// console.log(userNameForRendering); // "Dmytro"
+// console.log(userNameForRendering2); // false
+
+// user && <div></div>
+
+// console.log(Math.random());
+// console.log(Math.floor(3.1));
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.ceil(4.9));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.6));
+// console.log(Math.trunc(10.185));
+
+// const num = 10.185;
+
+// console.log(num.toFixed(1.4));
+// console.log(Number(num.toFixed(2)));
+// console.log();
+
+// const user = "Max";
+
+// console.log(user.length);
+// console.log("I\nWas\nBorn\nIn\nA\nHurricane");
+// console.log("Пам'ятати");
+
+// const text = "The Best Is yet To Come";
+// console.log(text.length);
+
+// console.log(text[4]);
+// console.log(text.charAt(5));
+
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
