29e5aa975e224bcfdc67e7a47965c4e4dd0d948b
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-08-22 23:39:07 +0300

    add workshop calculator

--- app.js
+++ app.js
@@ -1,161 +1,151 @@
 "use strict";
 
-// оголошуємо змінні з якими будемо працювати
-const form = document.querySelector(".create-task-form");
-const taskInput = document.querySelector(".task-input");
-const filterInput = document.querySelector(".filter-input");
-const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-tasks");
+// оголошуємо змінні
+const input = document.querySelector('.input');
+const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+const clear = document.querySelector('.clear');
+const result = document.querySelector('.result');
 
-// слухачі подій
-// запускаємо функцію showTasks коли весь HTML загружений
-document.addEventListener("DOMContentLoaded", showTasks);
-// запускаємо функцію addTask коли відправляємо форму (клікаємо на кнопку "Додати завдання")
-form.addEventListener("submit", addTask);
-// запускаємо функцію deleteTask коли клік попадає на список <ul>
-taskList.addEventListener("click", deleteTask);
-// запускаємо функцію після кліку на кнопку "Видалити всі елементи"
-clearBtn.addEventListener("click", deleteAllTasks);
-// запускаємо функцію filterTasks після того як ввідпускаємо клавішу (тоді, коли фокус в інпуті "Пошук завдань")
-filterInput.addEventListener("keyup", filterTasks);
+const operatorsList = ["+", "-", "×", "÷", '.'];
 
-function showTasks() {
-  // перевіряємо чи є у localStorage вже якісь завдання
-  if (localStorage.getItem("tasks")) {
-    // якщо вони там є - витягуємо їх і присвоюємо змінній
-    const tasks = JSON.parse(localStorage.getItem("tasks"));
+let isResultDisplyed = false;
 
-    // для кожної задачі яка є
-    tasks.forEach((task) => {
-      // створюємо елемент списку
-      const li = document.createElement("li");
-      // всередині цього елементу списку додаємо опис завдання
-      li.innerHTML = task;
+// додаємо обробники подій для всіх кнопок з математичними операторами
+operators.forEach((operator) => {
+  operator.addEventListener('click', (event) => {
+    // отримуємо доступ до значення всередині інпута
+    const valueInput = input.innerHTML;
+    // отримуємо доступ до останнього елемента інпута (останній елемент строки)
+    const lastChar = valueInput[valueInput.length - 1];
 
-      // сторюємо кнопку для видалення
-      const button = document.createElement("button");
-      // всередину кнопку додаємо значення х
-      button.innerHTML = "x";
-      // додаємо їй клас
-      button.classList.add("btn-delete");
-      // записуємо кнопку після всього, що є всередині елементу списку
-      li.append(button);
+    // якщо строка пуста, оператор не додаємо, код зупиняється
+    if (valueInput.length === 0) {
+      return null;
 
-      // записуємо цей елемент в кінець списку
-      taskList.append(li);
-    });
-  }
-}
+    // якщо в останній елемент в інпуті це оператор, то перезаписуємо його
+    } else if (operatorsList.includes(lastChar)) {
+      const newString = valueInput.substring(0, valueInput.length - 1) + event.target.innerHTML;
+      input.innerHTML = newString
 
-// створюємо таску
-function addTask(event) {
-  // зупиняємо поведінку браузера за замовчуванням
-  event.preventDefault();
-
-  // отримуємо значення з інпута через форму
-  const value = event.target.task.value;
-
-  // повторюємо всі дії з функції showTasks
-  const li = document.createElement("li");
-  li.innerHTML = value;
-
-  const button = document.createElement("button");
-  button.classList.add("btn-delete");
-  button.innerHTML = "x";
-
-  li.append(button);
-  taskList.append(li);
-
-  // але тут ще записуємо задачу в локал сторедж
-  storeTaskInLocalStorage(value);
-  // і чистимо інпут
-  taskInput.value = "";
-}
-
-function storeTaskInLocalStorage(task) {
-  // оголошуємо змінну яка буде використовуватись для списку завдань
-  let tasks;
-
-  // перевіряємо чи є у localStorage вже якісь завдання
-  if (localStorage.getItem("tasks") !== null) {
-    // якщо вони там є - витягуємо їх і присвоюємо змінній
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  } else {
-    // якщо їх там нема - присвоюємо змінній значення порожнього масиву
-    tasks = [];
-  }
-
-  // додаємо до списку нове завдання
-  tasks.push(task);
-
-  // зберігаємо список завданнь в Local Storage
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-// видалити якусь конкретну таску
-function deleteTask(event) {
-  // якщо ми клікнули по хрестику  - тоді
-  if (event.target.classList.contains("btn-delete")) {
-    // пересвідчуємось чи юзер справді хоче видалити цей елемент
-    if (confirm("Ви впевнені що хочете видалити цю задачу?")) {
-      // видаляємо цей елемент списку, в якому знаходиться хрестик
-      event.target.closest("li").remove();
-      // викликаємо функцію яка буде видаляти завдання з Local Storage
-      removeTaskFromLocalStorage(
-        event.target.closest("li").firstChild.textContent
-      );
-    }
-  }
-}
-
-function removeTaskFromLocalStorage(taskContent) {
-  // перевіряємо чи є у localStorage вже якісь завдання
-  if (localStorage.getItem("tasks")) {
-    // якщо вони там є - витягуємо їх і присвоюємо змінній
-    const tasks = JSON.parse(localStorage.getItem("tasks"));
-
-    // фільтруємо таски і повертаємо ті, які проходять умову
-    const filteredTasks = tasks.filter((task) => task !== taskContent);
-
-    // запусиємо нові задачі в Local Storage
-    localStorage.setItem("tasks", JSON.stringify(filteredTasks));
-  }
-}
-
-// видалити всі таски
-function deleteAllTasks() {
-  if (confirm("Ви впевнені що хочете видалити всі задачі?")) {
-    // видаляємо весь контент всередині списку
-    taskList.innerHTML = "";
-    // видалити всі елементи з Local Storage
-    removeAllTasksFromLocalStorage();
-  }
-}
-
-function removeAllTasksFromLocalStorage() {
-  // чистимо Local Storage
-  localStorage.clear();
-  //   localStorage.removeItem("tasks");
-}
-
-function filterTasks(event) {
-  // отримуємо всі елементи списку
-  const tasks = taskList.querySelectorAll("li");
-  // отримуємо значення інпуту "Пошук завдань" і робимо його в нижньому регістрі
-  const searchQuery = event.target.value.toLowerCase();
-
-  // проходимось по кожному елементу завдань
-  tasks.forEach((task) => {
-    // отримуємо текст завдання
-    const taskValue = task.firstChild.textContent.toLowerCase();
-
-    // перевіряємо чи текст завдання має в собі значення інпута "Пошук завдань"
-    if (taskValue.includes(searchQuery)) {
-      // якщо має, то display = list-item
-      task.style.display = "list-item";
+    // в іншому випадку додаємо до строки
     } else {
-      // якщо ні - ховаємо це елемент списку
-      task.style.display = "none";
+      input.innerHTML += event.target.innerHTML;
     }
-  });
-}
+  })
+});
+
+// додамо обробники подій для всіх кнопок з цифрами
+numbers.forEach((number) => {
+  number.addEventListener(('click'), (event) => {
+
+    // отримуємо доступ до значення всередині інпута
+    const valueInput = input.innerHTML;
+    // отримуємо доступ до останнього елемента інпута (останній елемент строки)
+    const lastChar = valueInput[valueInput.length - 1];
+
+    // якщо юзер просто вводить число - то ми його додаємо до введеної операції
+    if (!isResultDisplyed) {
+      input.innerHTML += event.target.innerHTML;
+
+    // якщо юзер вводить число коли вже висвічується результат попередньої операції і оператор є останнім символом інпута
+		// введене число додаємо до інпута
+    } else if (isResultDisplyed && operatorsList.includes(lastChar)) {
+      input.innerHTML += event.target.innerHTML;
+      // ставимо позначку що це вже не результат попередньої введеної операції
+      isResultDisplyed = false;
+
+    // якщо юзер вводить число коли відображається результат попередньої операції
+    // ми перезаписуємо весь інпут новим числом
+    } else {
+      input.innerHTML = event.target.innerHTML;
+      // ставимо позначку що це вже не результат попередньої введеної операції
+      isResultDisplyed = false;
+    }
+  })
+});
+
+clear.addEventListener('click', () => {
+  // очищуємо значення в рядку
+  input.innerHTML = '';
+  // ставимо позначку що це вже не результат попередньої введеної операції
+  isResultDisplyed = false;
+});
+
+result.addEventListener('click', () => {
+  // отримуємо доступ до значення всередині інпута
+  const valueInput = input.innerHTML;
+   // отримуємо доступ до останнього елемента інпута (останній елемент строки)
+  const lastChar = valueInput[valueInput.length - 1];
+
+  // якщо юзер останнім символом ввів оператор і натискає на отримання результату
+  if (operatorsList.includes(lastChar)) {
+    return null;
+  }
+
+  // отримуємо масив всіх введених чисел      
+  const onlyNumbers = valueInput.split(/\+|\-|\×|\÷/g);
+  // отримуємо масив всіх введених операторів (їх завжди менше на один ніж введених чисел)
+  const onlyOperators = valueInput.replace(/[0-9]|\./g, '').split('');
+
+  // ділення
+    // приклад:
+    // 1+4*5-6-8/2 
+    // [1,4,5,6,8,2] - onlyNumbers
+    // [+,*,-,-,/] - onlyOperators
+    // dividerIndex = 4
+    // const nums = [1,4,5,6,8,2] - onlyNumbers;
+    // nums.splice(4, 2, 8 / 2) - де 4 - це dividerIndex, 2 - кількість елементів які потрібно замінити включно з dividerIndex, 8 / 2 - результат того, на що міняємо
+
+  // знаходимо перший оператор множення серед всіх операторів
+  let dividerIndex = onlyOperators.indexOf('÷');
+   // доки оператор ділення є у масиві операторів (onlyOperators)
+  while(dividerIndex !== -1) {
+    // знаходимо у масиві значення чисел які знаходяться на цьому ж індексі що й оператор і наступне число і ділимо їх
+    const result = onlyNumbers[dividerIndex] / onlyNumbers[dividerIndex + 1];
+    // і замінюємо їх на результат
+    onlyNumbers.splice(dividerIndex, 2, result);
+    // видаляємо цей оператор ділення з масиву операторів
+    onlyOperators.splice(dividerIndex, 1);
+    // знаходимо наступний оператор ділення серед всіх операторів
+    dividerIndex = onlyOperators.indexOf('÷');
+  }
+
+  // множення
+  let multiplyIndex = onlyOperators.indexOf('×');
+  while(multiplyIndex !== -1) {
+    const result = onlyNumbers[multiplyIndex] * onlyNumbers[multiplyIndex + 1];
+
+    onlyNumbers.splice(multiplyIndex, 2, result);
+    onlyOperators.splice(multiplyIndex, 1);
+
+    multiplyIndex = onlyOperators.indexOf('×');
+  }
+
+  // віднімання
+  let substractIndex = onlyOperators.indexOf('-');
+  while(substractIndex !== -1) {
+    const result = onlyNumbers[substractIndex] - onlyNumbers[substractIndex + 1];
+
+    onlyNumbers.splice(substractIndex, 2, result);
+    onlyOperators.splice(substractIndex, 1);
+
+    substractIndex = onlyOperators.indexOf('-');
+  }
+
+  // додавання
+  let sumIndex = onlyOperators.indexOf('+');
+  while(sumIndex !== -1) {
+    const result = parseFloat(onlyNumbers[sumIndex]) + parseFloat(onlyNumbers[sumIndex + 1]);
+
+    onlyNumbers.splice(sumIndex, 2, result);
+    onlyOperators.splice(sumIndex, 1);
+
+    sumIndex = onlyOperators.indexOf('+');
+  }
+
+  // замінюємо значення введеної операції на її результат
+  input.innerHTML = onlyNumbers[0];
+  // ставимо позначку що це вже результат введеної операції
+  isResultDisplyed = true;
+});
